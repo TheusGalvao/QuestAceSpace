2f05e3f46c370d945a09e58a97ba852e7a9fcd46
MatheusGalvao <matheusgalvao2203@gmail.com>
2025-07-24 17:02:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,100 @@
 
 // Suas perguntas aqui
 const perguntas = [
-  // Exemplo
   {
-    pergunta: "Qual é a capital do Brasil?",
-    correta: "Brasília",
-    opcoes: ["São Paulo", "Rio de Janeiro", "Brasília", "Salvador"],
-    imagem: "https://exemplo.com/imagem1.jpg"
+    imagem: "images/astronauta.png",
+    pergunta: "Quem eram os Guarus e como eles viviam?",
+    opcoes: [
+      "Eram pessoas que cortavam as árvores",
+      "Eram indígenas espertos e gentis, e viviam com a natureza",
+      "Eram pessoas que jogavam lixo nas ruas",
+      "Eram pessoas comuns só que com capa"
+    ],
+    correta: "Eram indígenas espertos e gentis, e viviam com a natureza"
   },
-  // Adicione mais perguntas
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "O ar costumava ser...",
+    opcoes: ["Limpo e fresco", "Cheio de poluição", "Muito abafado", "Cheiroso"],
+    correta: "Limpo e fresco"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "Como era a Terra dos Guarus há muito tempo, antes de se tornar Guarulhos?",
+    opcoes: [
+      "A noisy city with cars",
+      "A dry and empty place",
+      "Full of big buildings and cars",
+      "Full of nature and magic"
+    ],
+    correta: "Full of nature and magic"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "Por que Guarulhos está pedindo ajuda?",
+    opcoes: [
+      "Because it rains",
+      "Because it is sad",
+      "Because of pollution",
+      "Because t-rex"
+    ],
+    correta: "Because of pollution"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "O que é um “dirty river”?",
+    opcoes: [
+      { texto: "img1", imagem: "images/dirty.jpg" },
+      { texto: "img2", imagem: "images/river.jpg" },
+      { texto: "img3", imagem: "images/tree.jpeg" },
+      { texto: "img4", imagem: "images/background.png" }
+    ],
+    correta: "img1"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "Global warming is...",
+    opcoes: [
+      "Quando cuidamos bem da natureza e dos animais",
+      "Quando os rios estão limpos e as florestas felizes",
+      "Quando a Terra fica mais quente e o clima muda porque as pessoas esqueceram de cuidar da natureza",
+      "Quando erram a temperatura local"
+    ],
+    correta: "Quando a Terra fica mais quente e o clima muda porque as pessoas esqueceram de cuidar da natureza"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "What does “nature” give us?",
+    opcoes: [
+      "Noise and smoke",
+      "Robots and buildings",
+      "Cake and soda",
+      "Clean air and water"
+    ],
+    correta: "Clean air and water"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "Who are the new heroes?",
+    opcoes: [
+      "Adults from space",
+      "Children like me",
+      "Animals of the forest",
+      "Guitar Players"
+    ],
+    correta: "Children like me"
+  },
+  {
+    imagem: "images/astronauta.png",
+    pergunta: "How can you help Guarulhos?",
+    opcoes: [
+      "Becoming an Environmental Agent and taking care of the Earth",
+      "Throwing garbage on the floor",
+      "Cutting trees",
+      "Playing guitar"
+    ],
+    correta: "Becoming an Environmental Agent and taking care of the Earth"
+  }
 ];
 
 let respostas = [];
